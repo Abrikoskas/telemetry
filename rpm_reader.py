7e919ff45fc6ae437fefc4a71e55a69527a6d899
Danil <danilbis35@gmail.com>
2024-05-09 18:04:34 +0300

    added timeout to rpm reader

--- rpm_reader.py
+++ rpm_reader.py
@@ -1,10 +1,14 @@
 import serial
+from time import time
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.reset_input_buffer()
 
 def get_rpm():
+    start = time()
     while True:
+        if time() - start > 0.5:
+            return 0
         if ser.in_waiting > 0:
             rpm = float(ser.readline().decode('utf-8').rstrip())
             return rpm
